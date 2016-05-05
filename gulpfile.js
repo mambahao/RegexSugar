f7bb65b36c8fc9caa31753ae490a158f5d33423f
Justin Hao <justin@haoyufeng.com>
2016-05-05 18:11:58 +0800

    add changlog task and file

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const shell = require('gulp-shell');
 
+gulp.task('changelog', shell.task(['git changelog -f 0.0.2 -p -x > CHANGELOG.md']));
 gulp.task('docs', shell.task(['jsdoc2md --template README.hbs "lib/**/*.js" > README.md']));
 
 gulp.task('default', () => gulp.src(['test/*.js', 'test/**/*.js'], { read:false }).pipe(mocha({ reporter:'spec' })));
