1c2cda649383d092dcd8d6c8694005f8ba4e9b48
Justin Hao <justin@haoyufeng.com>
2016-05-05 20:46:53 +0800

    修正模板文件名称

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ const mocha = require('gulp-mocha');
 const shell = require('gulp-shell');
 
 gulp.task('changelog', shell.task(['git changelog -f 0.0.2 -p -x > CHANGELOG.md']));
-gulp.task('docs', shell.task(['jsdoc2md --template README.hbs "lib/**/*.js" > README.md']));
+gulp.task('docs', shell.task(['jsdoc2md --template template_readme.hbs "lib/**/*.js" > README.md']));
 
 gulp.task('default', () => gulp.src(['test/*.js', 'test/**/*.js'], { read:false }).pipe(mocha({ reporter:'spec' })));
